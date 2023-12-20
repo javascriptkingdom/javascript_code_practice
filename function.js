622b562e43fb10406db78da0aa620131d38e8407
JavaScript Kingdom <45004158+javascriptkingdom@users.noreply.github.com>
2023-12-20 07:28:32 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,4 @@
+# JavaScript Function Assignment: Analyzing Real-life Data
+
+## Objective:
+Create a JavaScript program that defines and utilizes functions to analyze a real-life dataset. The dataset is a list of transactions from an online store, and your task is to create functions that extract meaningful information from this dataset.
